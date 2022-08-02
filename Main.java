39aa3a2bc39d8b66927ef5203ed2b25dd3444221
Raya <raisat.bekbulatova1234@gmail.com>
2022-08-02 23:44:00 +0300

    Добавили файлы

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> intList2 = new ArrayList<>();
+        for (Integer i : intList) {
+            if ((i > 0) && ((i % 2) == 0)) {
+                intList2.add(i);
+            }
+        }
+        Collections.sort(intList2);
+        System.out.println(Arrays.deepToString(new List[]{intList2}));
+    }
+}
